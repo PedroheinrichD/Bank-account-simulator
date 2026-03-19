07c09f734b97d51c3701961ace2ce6278c7d8f00
Pedro Heinrich <pedroheinrich91@gmail.com>
2026-03-19 07:18:25 -0300

    implementa lógica de contas bancárias com POO e seleção dinâmica de usuários

--- script.js
+++ script.js
@@ -7,6 +7,9 @@ let saldo = document.querySelector("#saldo")
 
 
 
+
+
+
 class ContaBancaria {
     _saldo = 0
     _historico = []
@@ -25,7 +28,7 @@ class ContaBancaria {
 
 
     deposito(valor) { // função para depositar na contabancaria
-        if (isNaN(valor) || valor <= 0) return 
+        if (isNaN(valor) || valor <= 0) return
         if (typeof valor !== 'number' || valor <= 0) {
             return
         }
@@ -53,7 +56,7 @@ class ContaBancaria {
         }
 
         // verificação onde o valor não pode ser MAIOR que o saldo disponível 
-        if (valor > this._saldo) { 
+        if (valor > this._saldo) {
             alert('saldo insuficiente')
             return
         }
@@ -75,7 +78,7 @@ class ContaBancaria {
     atualizarHistorico() {
         let ul = document.querySelector('ul')
         ul.innerHTML = ''
-        
+
         // percorrendo o objeto dentro do array 
         for (let i = 0; i < this._historico.length; i++) {
             const element = this._historico[i];
@@ -132,15 +135,18 @@ class ContaBancaria {
 
 const usuario = new ContaBancaria('Pedro', 0); // instancia da classe , criando um OBJETO
 const usuario2 = new ContaBancaria('Maria', 0);// instancia da classe , criando um OBJETO
+const usuario3 = new ContaBancaria('Joao', 0);// instancia da classe , criando um OBJETO
 
-
-
+let contasDoBanco = {
+    "Maria": usuario2,
+    "Joao": usuario3
+}
 
 
 function deposito() {
     const valor = Number(input.value) // pegando valor de input
     usuario.deposito(valor)  // chamando a função dentro da classe para fazer o depósito 
-    saldo.innerText = `R$ ${usuario.saldo}` // atualizando o valor do saldo no html
+    saldo.innerText = `R$ ${usuario.saldo.toLocaleString('pt-BR')}` // atualizando o valor do saldo no html
     atualizarStatus() // status da conta 'conta zerada'
     usuario.atualizarHistorico() // atualizando o historico de transações , depósito / saque / transferência
     input.value = ''
@@ -149,7 +155,7 @@ function deposito() {
 function saque() {
     const valor = Number(input.value)// pegando valor de input
     usuario.sacar(valor)// chamando a função dentro da classe para fazer o Saque 
-    saldo.innerText = `R$ ${usuario.saldo}` // atualizando o valor do saldo no html
+    saldo.innerText = `R$ ${usuario.saldo.toLocaleString('pt-BR')}` // atualizando o valor do saldo no html
     atualizarStatus() // chamando a função para atualizar o html -> 'conta zerada'
     usuario.atualizarHistorico() // atualizando o historico de transações , depósito / saque / transferência
     input.value = '' // limpando o valor do input
@@ -167,13 +173,24 @@ function atualizarStatus() { // status da conta 'conta zerada'
 
 function Transferencia() {
     const valor = Number(input.value) // pegando valor de input
-    usuario.transferir(valor, usuario2)// transferindo valor digitado para -> Maria(usuario2)
+    const select = document.querySelector('#select') // pegando valor do select
+
+    let usuarioSelecionado = select.value // pegando o valor do select que retorna uma string ex: "Maria"
+    let usuarioTransferencia = contasDoBanco[usuarioSelecionado] // acessa o objeto correspondente ex: "Maria": usuario2, ou seja, acessa o objeto que tem aquela string
+
+
+    usuario.transferir(valor, usuarioTransferencia)// transferindo valor digitado para ex: "Maria"(usuario2)
     // usuario2 -> está passando o objeto completo 
 
-    saldo.innerText = `R$ ${usuario.saldo}`// atualizando o valor do saldo no html
+    saldo.innerText = `R$ ${usuario.saldo.toLocaleString('pt-BR')}`// atualizando o valor do saldo no html
     atualizarStatus()// chamando a função para atualizar o html -> 'conta zerada'
     usuario.atualizarHistorico() // atualizando o historico de transações , depósito / saque / transferência
     input.value = ''
+
+    console.log(usuario);
+    console.log(usuario2);
+    console.log(usuario3);
+    
 }
 
 
